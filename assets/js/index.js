d16158879b9de1b66cfc0ea4de1740c78f17b9a1
austinmpask <austinmpask@gmail.com>
2024-02-02 20:01:55 -0800

    done

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,8 +2,65 @@ import Board from "./board.js";
 
 let board = new Board(); // creates a new game board
 
-// Examine the grid of the game board in the browser console.
-// Create the UI of the game using HTML elements based on this grid.
 console.log(board.grid);
 
-// Your code here
+function removeListeners() {
+  document.querySelectorAll(".grid-square").forEach((gridItem) => {
+    gridItem.removeEventListener("click", handleClick);
+  });
+}
+
+function addListeners() {
+  document.querySelectorAll(".grid-square").forEach((gridItem) => {
+    gridItem.addEventListener("click", handleClick);
+  });
+}
+function handleClick(event) {
+  const gridItem = event.currentTarget;
+  const data = gridItem.getAttribute("data-ship");
+  const row = data[0];
+  const col = data[2];
+  const result = board.makeHit(row, col);
+
+  console.log(result);
+  if (result) {
+    gridItem.style.backgroundColor = "green";
+    gridItem.innerHTML = result;
+  } else {
+    gridItem.style.backgroundColor = "red";
+  }
+
+  if (board.isGameOver()) {
+    removeListeners();
+    alert("Game over or something");
+  }
+}
+
+function initBoard() {
+  const gridContainer = document.createElement("div");
+  gridContainer.id = "gridContainer";
+  gridContainer.classList.add("grid-container");
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      const gridSquare = document.createElement("div");
+      gridSquare.setAttribute("data-ship", i + "," + j);
+
+      gridSquare.classList.add("grid-square");
+      gridContainer.appendChild(gridSquare);
+    }
+  }
+
+  document.querySelector("body").appendChild(gridContainer);
+}
+document.addEventListener("DOMContentLoaded", (event) => {
+  const resetButton = document.getElementById("reset");
+  resetButton.addEventListener("click", () => {
+    board = new Board();
+    document.getElementById("gridContainer").remove();
+    initBoard();
+    addListeners();
+  });
+
+  initBoard();
+  addListeners();
+});
